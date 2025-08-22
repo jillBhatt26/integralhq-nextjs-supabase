b5c9064bb37302f73bc57ca0ef579c9a1db7bc9a
jill bhatt <bhattjill@gmail.com>
2025-08-22 22:04:02 +0530

    Added my and all posts filter

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const Loader = () => {
+    return (
+        <div className="max-w-full h-96 flex justify-center items-center">
+            <span className="loading loading-bars loading-xl my-auto"></span>
+        </div>
+    );
+};
+
+export default Loader;
--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -3,20 +3,41 @@ import Link from 'next/link';
 import { TPost } from '@/lib/types/posts';
 import { createClient } from '@/utils/supabase/client';
 import { useCallback, useEffect, useState } from 'react';
+import useAuthStore from '@/store/auth';
+import Loader from './Loader';
 
 const Posts = () => {
     // states
     const [posts, setPosts] = useState<TPost[]>([]);
     const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [showMyPostsOnly, setShowMyPostsOnly] = useState<boolean>(false);
+
+    // hooks
+    const authUser = useAuthStore(state => state.user);
 
     // callbacks
     const fetchPostsCB = useCallback(async () => {
+        if (!authUser) return;
+
+        setLoading(true);
+
         const supabase = createClient();
 
-        const res = await supabase
-            .from('posts')
-            .select()
-            .order('created_at', { ascending: false });
+        let res;
+
+        if (showMyPostsOnly) {
+            res = await supabase
+                .from('posts')
+                .select()
+                .eq('author_id', authUser.id)
+                .order('created_at', { ascending: false });
+        } else {
+            res = await supabase
+                .from('posts')
+                .select()
+                .order('created_at', { ascending: false });
+        }
 
         const { data, error, status } = res;
 
@@ -26,12 +47,16 @@ const Posts = () => {
             );
 
         if (status === 200 && data) setPosts(data);
-    }, []);
+
+        setLoading(false);
+    }, [showMyPostsOnly, authUser]);
 
     // effects
     useEffect(() => {
         fetchPostsCB();
     }, [fetchPostsCB]);
+
+    if (loading) return <Loader />;
 
     return (
         <div className="my-8 lg:my-14">
@@ -44,8 +69,23 @@ const Posts = () => {
             {posts.length > 0 ? (
                 <>
                     <h1 className="text-2xl text-center text-gray-300">
-                        Posts
+                        {showMyPostsOnly ? 'My' : 'All'} Posts
                     </h1>
+
+                    <div className="my-5 px-5">
+                        <select
+                            defaultValue={showMyPostsOnly ? 'my' : 'all'}
+                            className="select w-full md:max-w-60"
+                            onChange={e =>
+                                setShowMyPostsOnly(
+                                    e.target.value.toLowerCase() === 'my'
+                                )
+                            }
+                        >
+                            <option value="all">Show All Posts</option>
+                            <option value="my">Show My Posts</option>
+                        </select>
+                    </div>
 
                     <ul className="my-5">
                         {posts.map((post: TPost, idx: number) => (
